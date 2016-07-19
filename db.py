5ed2867ca6e17e1d6289c6bacd17f659ef5c0ce4
chucklapress <charleslapress@gmail.com>
2016-07-19 11:32:32 -0400

    first commit workflow strange working python projects in pycharm

---
+++ db.py
@@ -0,0 +1,48 @@
+import csv
+def new_user():
+    user_name = input("enter user ")
+    password = input("enter password ")
+    name = input("Enter your name ")
+    added_data = input("Enter new data ")
+    line = "{},{},{},{}\n".format(user_name, password, name, added_data)
+
+    with open("db_a.csv","a") as outfile:
+        outfile.write(line)
+
+
+#new_user()
+
+def search_db():
+    with open("db_a.csv","r") as infile:
+        data = csv.DictReader(infile,fieldnames=['user_name', 'password', 'name', 'added_data'])
+        for row in data:
+            print(row['password'])
+
+
+def log_in_db():
+    user_is = input('enter user_name' ' ')
+    password = input('enter password'' ')
+
+    with open("db_a.csv", "r") as infile:
+        data = csv.DictReader(infile, fieldnames=['user_name', 'password', 'name', 'added_data'])
+        for row in data:
+            if user_is == row['user_name'] and password == row['password']:
+                print('success you are logged in')
+            elif user_is != row['user_name'] and password == row['password']:
+                print('Please retry log in')
+                return log_in_db()
+            elif user_is == row['user_name'] and password != row['password']:
+                print('Please retry logging in')
+                return log_in_db()
+
+
+
+
+
+
+    print(user_is)
+    print(password)
+
+
+search_db()
+log_in_db()
